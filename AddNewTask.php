a856dece0a12c9990b3cc1bca8279a4b51ca8a56
RyanPWebb <fly20webb@att.net>
2023-11-25 13:44:03 -0600

    fixed task addition

--- AddNewTask.php
+++ AddNewTask.php
@@ -1,22 +1,44 @@
 <?php
-    $title = "{$_POST["title"]}<br>";
-    $day = "{$_POST["Day"]} <br>";
-    $desc = "{$_POST["Description"]} <br>";
-    $time = "{$_POST["Time"]} <br>";
-    //Creates a new connection with the server
+    session_start();
 
-    $con = new mysqli('localhost', 'John Doe', 'password', 'Project');
-    
-    //Checks to make sure the connection is valid
-    if($con->connect_error)
-    {
-        echo "Connection failed: " . $con->conect_error;
-        exit();
+    $uid = $_SESSION['userID'];
+
+    $db_server = "localhost";
+    $db_user = "project";
+    $db_password = "Password123";
+    $db_name = "engineering_project";
+
+    $connection = "";
+
+    try {
+        $connection = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+    } catch (mysqli_sql_exception $ex) {
+        echo "Couldn't connect to database";
     }
-    $sql = "INSERT INTO Events (u_id,title,e_date,e_time,description) VALUES (1,$title,$day,$time, $desc)"; 
 
-    $result =$con->query($sql);
+    if (isset($_POST['title']) && isset($_POST['Day']) && isset($_POST['Description']) && isset($_POST['Time'])){
+        $title = $_POST["title"];
+        $day = $_POST["Day"];
+        $desc = $_POST["Description"];
+        $time = $_POST["Time"];
+        
 
+        $sql = "INSERT INTO Events (u_id, title, e_date, e_time, description) VALUES ('$uid', '$title', '$day', '$time', '$desc')";
+        
+        try {
+            mysqli_query($connection, $sql);
+            echo "<br> Task Created! <br>";
+
+        } catch (mysqli_sql_exception $ex) {
+            echo "<br> Unable to create event: '$ex'<br>";
+        }
+
+    }
+    else{
+        echo ' <br> Invalid Input <br>';
+    }
+
+    mysqli_close($connection);
 
 ?>
 <!DOCTYPE html>
@@ -32,7 +54,7 @@
         <label> Title of event:  </label> <br>
         <input type = "text" name = "title"> <br>
         <label> Time of event:  </label> <br>
-        <input type = "text" name = "title"> <br>
+        <input type = "time" name = "Time" min = "00:00" max = "23:59"> <br>
         <label> Day of event: </label> <br>
         <input type = "text" name = "Day"> <br>
         <label> Description: </label> <br>
